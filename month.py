d7b41ca77fa1a60f067a2b5f87cb0f2897e7233e
qiuchunxiao <qiucx@create80.com>
2018-12-07 14:22:23 +0800

    test

--- month.py
+++ month.py
@@ -1,16 +1,16 @@
 import datetime
 
-begin_str = '2018-10-01 06:00:00'
-end_str = '2018-10-31 06:00:00'
+begin_str = '2018-11-01 00:00:00'
+end_str = '2018-11-20 00:00:00'
 begin_dt = datetime.datetime.strptime(begin_str, '%Y-%m-%d %H:%M:%S')
 end_dt = datetime.datetime.strptime(end_str, '%Y-%m-%d %H:%M:%S')
 day_list = []
-while begin_dt <= end_dt:
+while begin_dt < end_dt:
     day_str = str(begin_dt)
     day_list.append(day_str)
-    begin_dt = begin_dt + datetime.timedelta(days=1)
+    begin_dt = begin_dt + datetime.timedelta(hours=1)
 for begin_str in day_list:
-    begintime=datetime.datetime.strptime(begin_str, '%Y-%m-%d %H:%M:%S')
-    endtime=begintime + datetime.timedelta(hours=16)-datetime.timedelta(microseconds=1)
-    end_str=str(endtime)
-    print begintime,endtime
+    begintime = datetime.datetime.strptime(begin_str, '%Y-%m-%d %H:%M:%S')
+    endtime = begintime +datetime.timedelta(hours=1)-datetime.timedelta(microseconds=1)
+    end_str = str(endtime)
+    print begintime, endtime
